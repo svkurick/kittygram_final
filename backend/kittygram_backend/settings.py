a25dff45e4431897ed9154e722f0d1ebb1748534
Сергей Куриленко <sv.kurick@gmail.com>
2023-06-01 19:09:46 +0300

    Fix settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -99,7 +99,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
